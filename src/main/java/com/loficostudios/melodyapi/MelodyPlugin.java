deea50e7bcfad0793cc6563e5d992b2fe39e3ef4
Logan <tonierbobcat966@gmail.com>
2025-01-24 14:24:39 -0800

    Update MelodyPlugin.java

--- src/main/java/com/loficostudios/melodyapi/MelodyPlugin.java
+++ src/main/java/com/loficostudios/melodyapi/MelodyPlugin.java
@@ -1,45 +1,23 @@
 package com.loficostudios.melodyapi;
 
-import lombok.Getter;
+import com.loficostudios.melodyapi.gui.GuiManager;
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.logging.Level;
-
-@ApiStatus.ScheduledForRemoval(inVersion = "1.0")
-@Deprecated
 public abstract class MelodyPlugin<T extends MelodyPlugin<T>> extends JavaPlugin {
 
-//    @Getter
-//    private final ConfigManager<T> configManager = new ConfigManager<>();
-    public MelodyPlugin() {
+    protected GuiManager guiManager;
+
+    protected abstract void onStart();
+
+    @Override
+    public void onEnable() {
+        var gui = new GuiManager();
+        Bukkit.getPluginManager().registerEvents(gui, this);
+        onStart();
     }
 
-//    private static MelodyPlugin<?> instance;
-//
-//    @SuppressWarnings("unchecked")
-//    public static <T extends MelodyPlugin<T>> T getInstance() {
-//        return (T) instance;
-//    }
-//
-//    /**
-//     *
-//     * @param plugin is set in plugin constructor
-//     */
-//    protected void setInstance(T plugin) {
-//        instance = plugin;
-//        MelodyAPI.addPlugin(plugin);
-//        this.getServer().getLogger().log(Level.INFO, "[" + getName() + "] set instance");
-//    }
-//
-//    @Override
-//    public void onLoad() {
-//        if (instance == null) {
-//            this.getServer().getLogger().log(Level.SEVERE, "[" + getName() + "] instance is not set disabling...");
-//            Bukkit.getPluginManager().disablePlugin(this);
-//        }
-//    }
+    public GuiManager getGuiManager() {
+        return guiManager;
+    }
 }
